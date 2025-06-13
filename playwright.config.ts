5716d07ea8df595f53b103adfa46ab8a39ccc243
kabix-dk <kabix.dk@gmail.com>
2025-06-13 18:18:48 +0200

    e2e test config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig, devices } from "@playwright/test";
 
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(process.cwd(), ".env.test") });
+
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
@@ -18,7 +22,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:4321",
+    baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -41,7 +45,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:4321",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
 });
